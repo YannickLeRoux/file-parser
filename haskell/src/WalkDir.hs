2ec66b52ba75039698fb2ad70300e15e3a32bc36
Yannick <yannick.leroux@edf-re.com>
2021-04-13 19:24:20 -0700

    improvements

--- haskell/src/WalkDir.hs
+++ haskell/src/WalkDir.hs
@@ -1,5 +1,6 @@
 module WalkDir (listFilesDirFiltered) where
 
+import Control.Monad (unless)
 import Data.Foldable (traverse_)
 import Data.List
 import System.Directory.Tree
@@ -11,20 +12,30 @@ import System.Directory.Tree
 import System.FilePath (takeExtension)
 import System.IO (readFile)
 
+targetedWords :: [String]
+targetedWords =
+  ["debugger", "it.only("]
+
+targetedExtensions :: [String]
+targetedExtensions =
+  [".js", ".jsx", ".ts", ".tsx"]
+
 listFilesDirFiltered :: String -> IO ()
 listFilesDirFiltered path = do
+  putStrLn "Started..."
   _ :/ tree <- readDirectoryWith return path
   traverse_ scanFile $ filterDir myPred tree
   putStrLn "Done."
   where
     myPred (Dir ('.' : _) _) = False
-    myPred (File n _) = takeExtension n `elem` [".txt", ".js", ".jsx", ".ts", ".tsx"]
+    myPred (Dir "node_modules" _) = False
+    myPred (Dir "coverage" _) = False
+    myPred (File n _) = takeExtension n `elem` targetedExtensions
     myPred _ = True
 
 scanFile :: FilePath -> IO ()
 scanFile f = do
   file <- readFile f
   let parsedLines = zip [1 ..] (lines file)
-  targetedWords <- readFile "../words.txt"
-  let filteredLines = [l | l <- parsedLines, any (\w -> w `isInfixOf` snd l) (lines targetedWords)]
-  print ((0, f) : filteredLines)
+  let filteredLines = [l | l <- parsedLines, any (\w -> w `isInfixOf` snd l) targetedWords]
+  unless (null filteredLines) $ print ((0, f) : filteredLines)
